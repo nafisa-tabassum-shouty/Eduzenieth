e43139a99d3dc1e519c6cf9b5fc2878cde6ecd2f
nafisa-tabassum-shouty <anisahmed50@gmail.com>
2024-06-11 05:13:49 +0600

    some classes
    classes

---
+++ EduzeniethFinal/EduzeniethFinal/Models/Announcement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EduzeniethFinal.Models
+{
+    public class Announcement
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string PostedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public List<string> Comments { get; set; }
+    }
+}
---
+++ EduzeniethFinal/EduzeniethFinal/Models/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EduzeniethFinal.Models
+{
+    public class Class1
+    {
+        [Required]
+        public HttpPostedFileBase File { get; set; }
+        // Property to store the file path
+        public string FilePath { get; set; }
+        public string Posts { get; set; }
+    }
+}
