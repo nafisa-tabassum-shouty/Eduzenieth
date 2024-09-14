4df29ea7ce9cfc4f600e95730d7545f4c3ba1eaf
nafisa-tabassum-shouty <anisahmed50@gmail.com>
2024-09-15 05:30:22 +0600

    question and result model

---
+++ EduzeniethFinal/EduzeniethFinal/Models/ResultViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EduzeniethFinal.Models
+{
+    public class ResultViewModel
+    {
+        public string Question { get; set; }
+        public string SelectedAnswer { get; set; }
+        public string CorrectAnswer { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+
+}
